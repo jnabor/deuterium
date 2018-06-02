f17a24570feb8099588fd0c76ce6c931645cf6ab
Jayson Nabor <sonabstudios@gmail.com>
2018-06-02 13:30:05 +0800

    - initial block class

---
+++ block.js
@@ -0,0 +1,19 @@
+class Block {
+    constructor(timeStamp, lastHash, hash, data) {
+        this.timeStamp = timeStamp
+        this.lastHash = lastHash
+        this.hash = hash
+        this.data = data
+    }
+
+    toString() {
+        return `Block -
+            Timestamp: ${this.timeStamp}
+            Last Hash: ${this.lastHash.substring(0,10)}
+            Hash:      ${this.hash.substring(0,10)}
+            Data:      ${this.data}
+        `
+    }
+}
+
+module.exports = Block;
